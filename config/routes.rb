dbb91f3385916112f9326ea9e6aeaf90c9c67ca4
Alina Bich <alina.a.bich@gmail.com>
2023-02-23 16:37:54 +0100

    bookmarks controller and view, routes do not work yet

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "lists#index"
-  resources :lists
+  resources :lists do
+    resources :bookmarks, only: %i[new create destroy]
+  end
 end
